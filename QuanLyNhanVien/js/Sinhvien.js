9ef201c0ee6cef1224c919854fd4f630ad77ebb0
ducthuan109 <ducthuan1022@gmail.com>
2023-08-22 14:39:53 +0700

    baitap

---
+++ QuanLyNhanVien/js/Sinhvien.js
@@ -0,0 +1,12 @@
+function NhanVien(taiKhoan, hoTen, email, matKhau, ngayLam, luongCoBan, chucVu, gioLamTrongThang) {
+    this.taiKhoan = taiKhoan;
+    this.hoTen = hoTen;
+    this.email = email;
+    this.matKhau = matKhau;
+    this.ngayLam = ngayLam;
+    this.luongCoBan = luongCoBan;
+    this.chucVu = chucVu;
+    this.gioLamTrongThang = gioLamTrongThang;
+    this.tongLuong = 0; 
+    this.loaiNhanVien = ""; 
+}
